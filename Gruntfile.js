f2e90037b4705b0f960adeca90ca257ad6c4f6ba
rgwch <weirich@elexis.ch>
2017-07-08 15:53:40 +0200

    initial import

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,15 +13,15 @@ module.exports = function (grunt) {
     var iopackage = grunt.file.readJSON('io-package.json');
     var version   = (pkg && pkg.version) ? pkg.version : iopackage.common.version;
     var newname   = grunt.option('name');
-    var author    = grunt.option('author') || '@@Author@@';
-    var email     = grunt.option('email')  || '@@email@@';
+    var author    = grunt.option('author') || 'Gerry Weirich';
+    var email     = grunt.option('email')  || 'weirich@elexis.ch';
     var fs        = require('fs');
 
     // check arguments
     if (process.argv[2] == 'rename') {
 		console.log('Try to rename to "' + newname + '"');
         if (!newname) {
-            console.log('Please write the new template name, like: "grunt rename --name=mywidgetset" --author="Author Name"');
+            console.log('Please write the new mystrom name, like: "grunt rename --name=mywidgetset" --author="Author Name"');
             process.exit();
         }
         if (newname.indexOf(' ') != -1) {
@@ -32,17 +32,17 @@ module.exports = function (grunt) {
             console.log('Name must be lower case.');
             process.exit();
         }
-        if (fs.existsSync(__dirname + '/admin/template.png')) {
-            fs.renameSync(__dirname + '/admin/template.png',              __dirname + '/admin/' + newname + '.png');
+        if (fs.existsSync(__dirname + '/admin/mystrom.png')) {
+            fs.renameSync(__dirname + '/admin/mystrom.png',              __dirname + '/admin/' + newname + '.png');
         }
-        if (fs.existsSync(__dirname + '/widgets/template.html')) {
-            fs.renameSync(__dirname + '/widgets/template.html',           __dirname + '/widgets/' + newname + '.html');
+        if (fs.existsSync(__dirname + '/widgets/mystrom.html')) {
+            fs.renameSync(__dirname + '/widgets/mystrom.html',           __dirname + '/widgets/' + newname + '.html');
         }
-        if (fs.existsSync(__dirname + '/widgets/template/js/template.js')) {
-            fs.renameSync(__dirname + '/widgets/template/js/template.js', __dirname + '/widgets/template/js/' + newname + '.js');
+        if (fs.existsSync(__dirname + '/widgets/mystrom/js/mystrom.js')) {
+            fs.renameSync(__dirname + '/widgets/mystrom/js/mystrom.js', __dirname + '/widgets/mystrom/js/' + newname + '.js');
         }
-        if (fs.existsSync(__dirname + '/widgets/template')) {
-            fs.renameSync(__dirname + '/widgets/template',                __dirname + '/widgets/' + newname);
+        if (fs.existsSync(__dirname + '/widgets/mystrom')) {
+            fs.renameSync(__dirname + '/widgets/mystrom',                __dirname + '/widgets/' + newname);
         }
     }
 
@@ -100,19 +100,19 @@ module.exports = function (grunt) {
                 options: {
                     patterns: [
                         {
-                            match: /template/g,
+                            match: /mystrom/g,
                             replacement: newname
                         },
                         {
-                            match: /Template/g,
-                            replacement: newname ? (newname[0].toUpperCase() + newname.substring(1)) : 'Template'
+                            match: /Mystrom/g,
+                            replacement: newname ? (newname[0].toUpperCase() + newname.substring(1)) : 'Mystrom'
                         },
                         {
-                            match: /@@Author@@/g,
+                            match: /Gerry Weirich/g,
                             replacement: author
                         },
                         {
-                            match: /@@email@@/g,
+                            match: /weirich@elexis.ch/g,
                             replacement: email
                         }
                     ]
